6ca701d46aa108f7fe5b079e9e362010d011f95e
Ezqull <259167@student.pwr.edu.pl>
2023-07-06 18:44:03 +0200

    some changes

--- pdf_creator.py
+++ pdf_creator.py
@@ -1,27 +1,35 @@
 from fpdf import FPDF
-
-pdf = FPDF('P', 'mm', 'A4')
+from datetime import datetime
 
 class PDF(FPDF):
-    def create(dict) -> None:
-        pdf.set_font('Helvetica', '', 30)
+    def create(self, dict) -> None:
+        self.set_font('Helvetica', '', 30)
 
-        pdf.add_page()
+        self.add_page()
 
-        pdf.cell(75, 10, '', ln = 0) 
-        pdf.cell(55, 10, 'SKUP', ln = 0) 
-        pdf.cell(70, 10, 'SPRZEDAZ', ln = 1)
+        self.cell(75, 10, '', ln = 0) 
+        self.cell(55, 10, 'SKUP', ln = 0) 
+        self.cell(70, 10, 'SPRZEDAZ', ln = 1)
 
-        waluty = ['Albania', 'es']
+        i = 1
+        pdf_name=''
+        print(dict)
+        for key, value in dict.items():
+            self.rect(4.8, (i*45) - 15.2, 40.4, 25.4, 'F')
+            self.image(value[0], 5, (i*45) - 15, 40, 25)
+            self.set_font('helvetica', '', 30)
+            self.text(50, i*45, f'{key}')
+            self.set_font('helvetica', '', 40)
+            self.text(85, i*45, f'{value[1]}')
+            self.text(145, i*45, f'{value[2]}')
+            pdf_name += key+"_"
+            i+=1
 
-        for i in range(1, len(waluty)+1):
-            pdf.image('Albania.png', 5, (i*45) - 15, h=25)
-            pdf.set_font('helvetica', '', 30)
-            pdf.text(45, i*45, 'ALL')
-            pdf.set_font('helvetica', '', 40)
-            pdf.text(80, i*45, '0,0435')
-            pdf.text(145, i*45, '0,0485')
-        
-        pdf.output('ELO.pdf')
+        now = datetime.now()
+        date = now.strftime("%d-%m-%Y")
+        pdf_name += date+".pdf"
+        self.output(pdf_name)
+
+        return pdf_name
 
 
---
+++ static/js/script.js
@@ -0,0 +1,51 @@
+const selectBtn = document.querySelector(".select-btn"), 
+        items = document.querySelectorAll(".item"), 
+        addBtn = document.querySelector(".add-from-list-btn");
+
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
+});
+
+let list = []
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("checked");
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".curr-btn");
+
+        if(item.classList.contains("checked")) {
+            list.push(item.innerText)
+        } else {
+            for(let i = 0; i < list.length; i++){
+                if(list[i] == item.innerText){
+                    list.splice(i, i+1);
+                }
+            }
+        }
+
+        if(list.length == 0) {
+            btnText.innerText = "Select";
+        }else {
+            btnText.innerText = list;
+        }
+    })
+})
+
+addBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let form = document.createElement("form");
+    let element1 = document.createElement("input");
+
+    form.method = "POST";
+    form.action = "/create-curr-pdf";  
+
+    element1.value = list;
+    element1.name = "currencies";
+    form.appendChild(element1);
+
+    document.body.appendChild(form);
+
+    form.submit();
+});
